ef8ed1e885456ce9e7f354c3afd8d0a952052619
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-08-31 02:08:19 -0300

    feat: alternado endpoint da api

--- src/components/React/RenderBooks.tsx
+++ src/components/React/RenderBooks.tsx
@@ -34,7 +34,7 @@ export const RenderBooks = () => {
     useEffect(() => {
         const getBooks = async () => {
             try {
-                const url = `http://localhost:1337/api/books`
+                const url = `https://capable-cuddle-84d83ce599.strapiapp.com/api/books`
                 const { data } = await axios(url)
                 setBooks(data.data.map((book: any) => ({ ...book.attributes, id: book.id })))
             } catch (err) {
@@ -52,7 +52,7 @@ export const RenderBooks = () => {
                 {themes.map((theme, index) => <div key={index} style={{ background: `#${theme.color}` }} className={`w-full bg-[#${theme.color}] rounded-xl py-2 px-8 cursor-pointer hover:opacity-70 font-semibold text-[16px] md:text-[18px] lg:text-[20px] 2xl:text-[26px] text-center`}>{theme.value} {index + 1}</div>)}
             </div>
             <div className='bg-white rounded-xl my-6 md:my-8 2xl:my-11'>
-                <div className='grid grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4'>
+                {books.length !== 0 ? (<div className='grid grid-cols-2 lg:grid-cols-4 2xl:grid-cols-4'>
                     {books.map((book, index) => <div key={index} className='flex flex-col items-center text-center'>
                         <div><CoverBookImg className='h-fit w-inherit max-w-[130px] sm:w-[180px] md:w-[210px] lg:w-[250px] sm:max-w-none 2xl:max-w-none 2xl:w-[340px]' /></div>
                         <div className=''>
@@ -61,7 +61,8 @@ export const RenderBooks = () => {
                             <div className='text-[#CFDA29] text-[14px] md:text-[18px] 2xl:text-[26px] font-semibold'>{formatPrice(book.price)}</div>
                         </div>
                     </div>)}
-                </div>
+                </div>)
+                : <div className='text-center py-6'>Nenhum livro encontrado.</div>}
                 <div className='py-6 w-full flex justify-center items-center'>
                     <div className='bg-[#EE8A21] cursor-pointer hover:opacity-70 transition-all rounded-full py-2 w-fit flex items-center text-white px-7'>
                         <div><FaPlus size={15} color='' /></div>
