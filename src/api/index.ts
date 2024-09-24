81add30c16d8e79ed02db7b4a3361007de8305b3
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-09-24 01:29:22 -0300

    feat: adicionando api ec2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:1337/api'
-    // baseURL: 'https://capable-cuddle-84d83ce599.strapiapp.com/api'
+    // baseURL: 'http://localhost:1337/api'
+    baseURL: 'http://ec2-18-118-102-139.us-east-2.compute.amazonaws.com:1337/api'
 });
 
 export default api;
