d72bac671702ec83e1ed2e1512bb48ba78ff4986
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-10-08 01:32:04 -0300

    feat: adicionando heroku endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:1337/api'
-    // baseURL: 'http://ec2-18-118-102-139.us-east-2.compute.amazonaws.com:1337/api'
+    // baseURL: 'http://localhost:1337/api'
+    baseURL: 'https://cranio-app-0b301e2c4ce6.herokuapp.com/'
 });
 
 export default api;
