446ba34456eebe3d734cb10a9de994e2a595d4af
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-10-20 22:26:35 -0300

    bugFix: corrigindo url da api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:1337/api',
-    baseURL: 'https://cranio-app-0b301e2c4ce6.herokuapp.com'
+    baseURL: 'https://cranio-app-0b301e2c4ce6.herokuapp.com/api'
     // headers: {
     //     'Content-Type': 'application/json',
     //     'Authorization': `Bearer ` + localStorage.getItem("jwt")
