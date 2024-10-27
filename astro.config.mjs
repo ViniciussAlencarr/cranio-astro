8dd4eff2d48d1c96a5475c6d0c9a638b03453d43
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-10-27 15:44:24 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   integrations: [react(), tailwind()],
 });
