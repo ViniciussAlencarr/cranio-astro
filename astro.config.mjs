862b8757c69757e303291aee29320690877e2263
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-10-27 15:45:35 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   integrations: [react(), tailwind()],
+  adapter: vercel()
 });
