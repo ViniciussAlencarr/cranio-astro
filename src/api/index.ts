297e4cae5eb04f4cd38545b8b98a5505b402b72b
ViniciussAlencarr <ViniciussAlencarGitHub@gmail.com>
2024-11-02 02:33:48 -0300

    fix: corrigindo problema com endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:1337/api',
-    baseURL: 'https://api.cranio.converter.tec.br/'
+    baseURL: 'https://api.cranio.converter.tec.br/api'
     // headers: {
     //     'Content-Type': 'application/json',
     //     'Authorization': `Bearer ` + localStorage.getItem("jwt")
